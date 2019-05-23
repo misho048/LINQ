460b57b7377a75a4f7996ba8c9fe7f2a8c09120c
Jakub Berthoty <jakub.berthoty@gmail.com>
2019-05-23 22:41:38 +0200

    Projection.

---
+++ LINQ/Projection.cs
@@ -0,0 +1,138 @@
+﻿using LINQ.Data;
+using LINQ.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LINQ
+{
+    public static class Projection
+    {
+
+        /// <summary>
+        /// Returns sequence of ints one higher than those in an existing array of ints.
+        /// </summary>
+        /// <returns>Collection with ints one higher.</returns>
+        public static IEnumerable<int> Select01()
+        {
+            int[] numbers = { 5, 4, 1, 3, 9, 8, 6, 7, 2, 0 };
+
+            // !!! INSERT YOUR LINQ  MAGIC HERE !!!
+
+            return new int[] { };
+        }
+
+        /// <summary>
+        /// Returns sequence of just the names of a list of products.
+        /// </summary>
+        /// <returns>Collection with just the names of a list of products.</returns>
+        public static IEnumerable<string> Select02()
+        {
+            List<Product> products = DataLoader.GetProductList();
+
+            // !!! INSERT YOUR LINQ  MAGIC HERE !!!
+
+            return new string[] { };
+        }
+
+        /// <summary>
+        /// Returns sequence of strings representing the text version of a sequence of ints.
+        /// </summary>
+        /// <returns>Collection with strings representing the text version of a sequence of ints.</returns>
+        public static IEnumerable<string> SelectTransformation()
+        {
+            int[] numbers = { 5, 4, 1, 3, 9, 8, 6, 7, 2, 0 };
+            string[] strings = { "zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine" };
+
+            // !!! INSERT YOUR LINQ  MAGIC HERE !!!
+
+            return new string[] { };
+        }
+
+        /// <summary>
+        /// Returns sequence of the uppercase and lowercase versions of each word in the original array.
+        /// </summary>
+        /// <returns>Output for each item in following format: "Uppercase: {item.upper}, Lowercase: {item.lower}"</returns>
+        public static IEnumerable<string> SelectAnonymousTypes01()
+        {
+            string[] words = { "aPPLE", "BlUeBeRrY", "cHeRry" };
+
+            // !!! INSERT YOUR LINQ  MAGIC HERE !!!
+            // Create LINQ query and replace anonymousItems array with its results
+            // Results should be anonymous types with string Upper and string Lower properties
+            dynamic[] anonymousItems = { new { Upper = string.Empty, Lower = string.Empty } };
+            foreach (dynamic item in anonymousItems)
+            {
+                yield return $"Uppercase: {item.Upper}, Lowercase: {item.Lower}";
+            }
+        }
+
+        /// <summary>
+        /// Returns sequence containing text representations of digits and whether they are even or odd.
+        /// </summary>
+        /// <returns>Output for each item in following format: "The digit {item.Digit} is {item.EvenOdd}."</returns>
+        public static IEnumerable<string> SelectAnonymousTypes02()
+        {
+            int[] numbers = { 5, 4, 1, 3, 9, 8, 6, 7, 2, 0 };
+            string[] strings = { "zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine" };
+
+            // !!! INSERT YOUR LINQ  MAGIC HERE !!!
+            // Create LINQ query and replace anonymousItems array with its results
+            // Results should be anonymous types with string Digit and bool Even properties
+            dynamic[] anonymousItems = { new { Digit = string.Empty, Even = false } };
+            foreach (dynamic item in anonymousItems)
+            {
+                yield return $"The digit {item.Digit} is {(item.Even ? "even" : "odd")}.";
+            }
+        }
+
+        /// <summary>
+        /// Returns sequence containing information about whether the values of ints in an array match their position in the array.
+        /// </summary>
+        /// <returns>Output for each item in following format: "{item.Digit}: {item.InPlace}."</returns>
+        public static IEnumerable<string> SelectIndexed()
+        {
+            int[] numbers = { 5, 4, 1, 3, 9, 8, 6, 7, 2, 0 };
+
+            // !!! INSERT YOUR LINQ  MAGIC HERE !!!
+            // Create LINQ query and replace anonymousItems array with its results
+            // Results should be anonymous types with int Digit and bool InPlace properties
+            dynamic[] anonymousItems = { new { Digit = -1, InPlace = false } };
+            foreach (dynamic item in anonymousItems)
+            {
+                yield return $"{item.Digit}: {item.InPlace}";
+            }
+        }
+
+        /// <summary>
+        /// Returns sequence of the text form of each digit from an array less than 5.
+        /// </summary>
+        /// <returns>Collection with the text form of each digit from an array less than 5.</returns>
+        public static IEnumerable<string> SelectFiltered()
+        {
+            int[] numbers = { 5, 4, 1, 3, 9, 8, 6, 7, 2, 0 };
+            string[] digits = { "zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine" };
+
+            // !!! INSERT YOUR LINQ  MAGIC HERE !!!
+
+            return new string[] { };
+        }
+
+        /// <summary>
+        /// Returns sequence containing customers from Washington (WA) region and IDs of their orders placed on or after 1.1.1997.
+        /// </summary>
+        /// <returns>Output for each item in following format: "CustomerID={item.CustomerId} OrderID={item.OrderId}"</returns>
+        public static IEnumerable<string> SelectMany()
+        {
+            List<Customer> customers = DataLoader.GetCustomerList();
+
+            // !!! INSERT YOUR LINQ  MAGIC HERE !!!
+            // Create LINQ query and replace anonymousItems array with its results
+            // Results should be anonymous types with string CustomerId and int OrderId properties
+            dynamic[] anonymousItems = { new { CustomerId = string.Empty, OrderId = -1 } };
+            foreach (dynamic item in anonymousItems)
+            {
+                yield return $"CustomerID={item.CustomerId} OrderID={item.OrderId}";
+            }
+        }
+    }
+}
---
+++ LinqTests/ProjectionTest.cs
@@ -0,0 +1,150 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using LINQ;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LinqTests
+{
+    [TestClass]
+    public class ProjectionTest
+    {
+        [TestMethod]
+        public void TestSelect01()
+        {
+            IEnumerable<int> actual = Projection.Select01();
+            IEnumerable<int> expected = new int[] { 6, 5, 2, 4, 10, 9, 7, 8, 3, 1 };
+
+            CollectionAssert.AreEqual(actual.ToList(), expected.ToList(), "You failed!");
+        }
+
+        [TestMethod]
+        public void TestSelect02()
+        {
+            IEnumerable<string> actual = Projection.Select02();
+            IEnumerable<string> expected =
+                new string[] {
+                    "Chai", "Chang", "Aniseed Syrup", "Chef Anton's Cajun Seasoning", "Chef Anton's Gumbo Mix",
+                    "Grandma's Boysenberry Spread", "Uncle Bob's Organic Dried Pears", "Northwoods Cranberry Sauce",
+                    "Mishi Kobe Niku", "Ikura", "Queso Cabrales", "Queso Manchego La Pastora", "Konbu",
+                    "Tofu", "Genen Shouyu", "Pavlova", "Alice Mutton", "Carnarvon Tigers", "Teatime Chocolate Biscuits",
+                    "Sir Rodney's Marmalade", "Sir Rodney's Scones", "Gustaf's Knäckebröd",  "Tunnbröd",
+                    "Guaraná Fantástica", "NuNuCa Nuß-Nougat-Creme", "Gumbär Gummibärchen", "Schoggi Schokolade",
+                    "Rössle Sauerkraut", "Thüringer Rostbratwurst", "Nord-Ost Matjeshering", "Gorgonzola Telino",
+                    "Mascarpone Fabioli", "Geitost", "Sasquatch Ale", "Steeleye Stout", "Inlagd Sill",
+                    "Gravad lax", "Côte de Blaye", "Chartreuse verte", "Boston Crab Meat", "Jack's New England Clam Chowder",
+                    "Singaporean Hokkien Fried Mee", "Ipoh Coffee", "Gula Malacca", "Rogede sild", "Spegesild",
+                    "Zaanse koeken", "Chocolade", "Maxilaku", "Valkoinen suklaa", "Manjimup Dried Apples",
+                    "Filo Mix", "Perth Pasties", "Tourtière", "Pâté chinois", "Gnocchi di nonna Alice",
+                    "Ravioli Angelo", "Escargots de Bourgogne", "Raclette Courdavault", "Camembert Pierrot",
+                    "Sirop d'érable", "Tarte au sucre", "Vegie-spread", "Wimmers gute Semmelknödel",
+                    "Louisiana Fiery Hot Pepper Sauce", "Louisiana Hot Spiced Okra", "Laughing Lumberjack Lager",
+                    "Scottish Longbreads", "Gudbrandsdalsost", "Outback Lager", "Flotemysost", "Mozzarella di Giovanni",
+                    "Röd Kaviar", "Longlife Tofu", "Rhönbräu Klosterbier", "Lakkalikööri", "Original Frankfurter grüne Soße"
+                };
+
+            CollectionAssert.AreEqual(actual.ToList(), expected.ToList(), "You failed!");
+        }
+
+        [TestMethod]
+        public void TestSelectTransformation()
+        {
+            IEnumerable<string> actual = Projection.SelectTransformation();
+            IEnumerable<string> expected = new string[] { "five", "four", "one", "three", "nine", "eight", "six", "seven", "two", "zero" };
+
+            CollectionAssert.AreEqual(actual.ToList(), expected.ToList(), "You failed!");
+        }
+
+        [TestMethod]
+        public void TestSelectAnonymousTypes01()
+        {
+            IEnumerable<string> actual = Projection.SelectAnonymousTypes01();
+            IEnumerable<string> expected =
+                new string[] {
+                    "Uppercase: APPLE, Lowercase: apple",
+                    "Uppercase: BLUEBERRY, Lowercase: blueberry",
+                    "Uppercase: CHERRY, Lowercase: cherry"
+                };
+
+            CollectionAssert.AreEqual(actual.ToList(), expected.ToList(), "You failed!");
+        }
+
+        [TestMethod]
+        public void TestSelectAnonymousTypes02()
+        {
+            IEnumerable<string> actual = Projection.SelectAnonymousTypes02();
+            IEnumerable<string> expected =
+                new string[] {
+                    "The digit five is odd.",
+                    "The digit four is even.",
+                    "The digit one is odd.",
+                    "The digit three is odd.",
+                    "The digit nine is odd.",
+                    "The digit eight is even.",
+                    "The digit six is even.",
+                    "The digit seven is odd.",
+                    "The digit two is even.",
+                    "The digit zero is even."
+                };
+
+            CollectionAssert.AreEqual(actual.ToList(), expected.ToList(), "You failed!");
+        }
+
+        [TestMethod]
+        public void TestSelectIndexed()
+        {
+            IEnumerable<string> actual = Projection.SelectIndexed();
+            IEnumerable<string> expected =
+                new string[] {
+                    "5: False",
+                    "4: False",
+                    "1: False",
+                    "3: True",
+                    "9: False",
+                    "8: False",
+                    "6: True",
+                    "7: True",
+                    "2: False",
+                    "0: False"
+                };
+
+            CollectionAssert.AreEqual(actual.ToList(), expected.ToList(), "You failed!");
+        }
+
+        [TestMethod]
+        public void TestSelectFiltered()
+        {
+            IEnumerable<string> actual = Projection.SelectFiltered();
+            IEnumerable<string> expected = new string[] { "four", "one", "three", "two", "zero" };
+
+            CollectionAssert.AreEqual(actual.ToList(), expected.ToList(), "You failed!");
+        }
+
+        [TestMethod]
+        public void TestSelectMany()
+        {
+            IEnumerable<string> actual = Projection.SelectMany();
+            IEnumerable<string> expected =
+                new string[] {
+                    "CustomerID=LAZYK OrderID=10482",
+                    "CustomerID=LAZYK OrderID=10545",
+                    "CustomerID=TRAIH OrderID=10574",
+                    "CustomerID=TRAIH OrderID=10577",
+                    "CustomerID=TRAIH OrderID=10822",
+                    "CustomerID=WHITC OrderID=10469",
+                    "CustomerID=WHITC OrderID=10483",
+                    "CustomerID=WHITC OrderID=10504",
+                    "CustomerID=WHITC OrderID=10596",
+                    "CustomerID=WHITC OrderID=10693",
+                    "CustomerID=WHITC OrderID=10696",
+                    "CustomerID=WHITC OrderID=10723",
+                    "CustomerID=WHITC OrderID=10740",
+                    "CustomerID=WHITC OrderID=10861",
+                    "CustomerID=WHITC OrderID=10904",
+                    "CustomerID=WHITC OrderID=11032",
+                    "CustomerID=WHITC OrderID=11066"
+                };
+
+            CollectionAssert.AreEqual(actual.ToList(), expected.ToList(), "You failed!");
+        }
+    }
+}

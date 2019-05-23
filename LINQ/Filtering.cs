a264839b9d79c771c1745c77969b5ac4a55b4960
Jakub Berthoty <berthoty@kros.sk>
2019-05-23 13:34:54 +0200

    Filtering - where01 + test.

--- LINQ/Filtering.cs
+++ LINQ/Filtering.cs
@@ -1,12 +1,22 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
 
 namespace LINQ
 {
-    class Filtering
+    public static class Filtering
     {
+
+        /// <summary>
+        /// Finds all elements of an array less than 5.
+        /// </summary>
+        /// <returns>Collection with elements less than 5.</returns>
+        public static IEnumerable<int> Where01()
+        {
+            int[] numbers = { 5, 4, 1, 3, 9, 8, 6, 7, 2, 0 };
+
+
+
+            return new int[] { };
+        }
+
     }
 }
--- LinqTests/FilteringTest.cs
+++ LinqTests/FilteringTest.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using LINQ;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace LinqTests
 {
@@ -6,8 +9,12 @@ namespace LinqTests
     public class FilteringTest
     {
         [TestMethod]
-        public void TestMethod1()
+        public void TestWhere01()
         {
+            IEnumerable<int> actual = Filtering.Where01();
+            IEnumerable<int> expected = new int[] { 4, 1, 3, 2, 0 };
+
+            CollectionAssert.AreEqual(actual.ToList(), expected.ToList());
         }
     }
 }

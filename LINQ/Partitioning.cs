cd58287a77301864f3a695f96073613defec4780
Jakub Berthoty <berthoty@kros.sk>
2019-05-24 06:58:09 +0200

    Partitioning.

---
+++ LINQ/Partitioning.cs
@@ -0,0 +1,114 @@
+﻿using LINQ.Data;
+using LINQ.Models;
+using System.Collections.Generic;
+
+namespace LINQ
+{
+    public static class Partitioning
+    {
+
+        /// <summary>
+        /// Returns only first 3 elements of the array.
+        /// </summary>
+        /// <returns>Collection with first 3 elements of the array.</returns>
+        public static IEnumerable<int> Take()
+        {
+            int[] numbers = { 5, 4, 1, 3, 9, 8, 6, 7, 2, 0 };
+
+            // !!! INSERT YOUR LINQ  MAGIC HERE !!!
+
+            return new int[] { };
+        }
+
+        /// <summary>
+        /// Returns first 3 orders from customers in Washington (WA) region.
+        /// </summary>
+        /// <returns>Collection with first 3 orders from customers in Washington (WA) region.</returns>
+        public static IEnumerable<CustomerOrderDto> TakeNested()
+        {
+            List<Customer> customers = DataLoader.GetCustomerList();
+
+            // !!! INSERT YOUR LINQ  MAGIC HERE !!!
+
+            return new CustomerOrderDto[] { };
+        }
+
+        /// <summary>
+        /// Returns all but the first 4 elements of the array.
+        /// </summary>
+        /// <returns>Collection with all but the first 4 elements of the array.</returns>
+        public static IEnumerable<int> Skip()
+        {
+            int[] numbers = { 5, 4, 1, 3, 9, 8, 6, 7, 2, 0 };
+
+            // !!! INSERT YOUR LINQ  MAGIC HERE !!!
+
+            return new int[] { };
+        }
+
+        /// <summary>
+        /// Returns all but first 2 orders from customers in Washington (WA) region.
+        /// </summary>
+        /// <returns>Collection with all but first 2 orders from customers in Washington (WA) region.</returns>
+        public static IEnumerable<CustomerOrderDto> SkipNested()
+        {
+            List<Customer> customers = DataLoader.GetCustomerList();
+
+            // !!! INSERT YOUR LINQ  MAGIC HERE !!!
+
+            return new CustomerOrderDto[] { };
+        }
+
+        /// <summary>
+        /// Returns elements starting from the beginning of the array until a number is hit that is not less than 6.
+        /// </summary>
+        /// <returns>Collection with elements starting from the beginning of the array until a number is hit that is not less than 6.</returns>
+        public static IEnumerable<int> TakeWhile()
+        {
+            int[] numbers = { 5, 4, 1, 3, 9, 8, 6, 7, 2, 0 };
+
+            // !!! INSERT YOUR LINQ  MAGIC HERE !!!
+
+            return new int[] { };
+        }
+
+        /// <summary>
+        /// Returns elements starting from the beginning of the array until a number is hit that is less than its position in the array.
+        /// </summary>
+        /// <returns>Collection with elements starting from the beginning of the array until a number is hit that is less than its position in the array.</returns>
+        public static IEnumerable<int> TakeWhileIndexed()
+        {
+            int[] numbers = { 5, 4, 1, 3, 9, 8, 6, 7, 2, 0 };
+
+            // !!! INSERT YOUR LINQ  MAGIC HERE !!!
+
+            return new int[] { };
+        }
+
+        /// <summary>
+        /// Returns elements of the array starting from the first element divisible by 3.
+        /// </summary>
+        /// <returns>Collection with elements of the array starting from the first element divisible by 3.</returns>
+        public static IEnumerable<int> SkipWhile()
+        {
+            int[] numbers = { 5, 4, 1, 3, 9, 8, 6, 7, 2, 0 };
+
+            // !!! INSERT YOUR LINQ  MAGIC HERE !!!
+
+            return new int[] { };
+        }
+
+        /// <summary>
+        /// Returns elements of the array starting from the first element less than its position.
+        /// </summary>
+        /// <returns>Collection with elements of the array starting from the first element less than its position.</returns>
+        public static IEnumerable<int> SkipWhileIndexed()
+        {
+            int[] numbers = { 5, 4, 1, 3, 9, 8, 6, 7, 2, 0 };
+
+            // !!! INSERT YOUR LINQ  MAGIC HERE !!!
+
+            return new int[] { };
+        }
+    }
+}
---
+++ LinqTests/PartitioningTest.cs
@@ -0,0 +1,110 @@
+﻿using LINQ;
+using LINQ.Models;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LinqTests
+{
+    [TestClass]
+    public class PartitioningTest
+    {
+        [TestMethod]
+        public void TestTake()
+        {
+            IEnumerable<int> actual = Partitioning.Take();
+            IEnumerable<int> expected = new int[] { 5, 4, 1 };
+
+            CollectionAssert.AreEqual(actual.ToList(), expected.ToList(), "You failed!");
+        }
+
+        [TestMethod]
+        public void TestTakeNested()
+        {
+            IEnumerable<CustomerOrderDto> actual = Partitioning.TakeNested();
+            IEnumerable<CustomerOrderDto> expected =
+                new CustomerOrderDto[]
+                {
+                    new CustomerOrderDto() { CustomerId = "LAZYK", OrderId = 10482 },
+                    new CustomerOrderDto() { CustomerId = "LAZYK", OrderId = 10545 },
+                    new CustomerOrderDto() { CustomerId = "TRAIH", OrderId = 10574 }
+                };
+
+            CollectionAssert.AreEqual(actual.ToList(), expected.ToList(), "You failed!");
+        }
+
+        [TestMethod]
+        public void TestSkip()
+        {
+            IEnumerable<int> actual = Partitioning.Skip();
+            IEnumerable<int> expected = new int[] { 9, 8, 6, 7, 2, 0 };
+
+            CollectionAssert.AreEqual(actual.ToList(), expected.ToList(), "You failed!");
+        }
+
+        [TestMethod]
+        public void TestSkipNested()
+        {
+            IEnumerable<CustomerOrderDto> actual = Partitioning.SkipNested();
+            IEnumerable<CustomerOrderDto> expected =
+                new CustomerOrderDto[]
+                {
+                    new CustomerOrderDto() { CustomerId = "TRAIH", OrderId = 10574 },
+                    new CustomerOrderDto() { CustomerId = "TRAIH", OrderId = 10577 },
+                    new CustomerOrderDto() { CustomerId = "TRAIH", OrderId = 10822 },
+                    new CustomerOrderDto() { CustomerId = "WHITC", OrderId = 10269 },
+                    new CustomerOrderDto() { CustomerId = "WHITC", OrderId = 10344 },
+                    new CustomerOrderDto() { CustomerId = "WHITC", OrderId = 10469 },
+                    new CustomerOrderDto() { CustomerId = "WHITC", OrderId = 10483 },
+                    new CustomerOrderDto() { CustomerId = "WHITC", OrderId = 10504 },
+                    new CustomerOrderDto() { CustomerId = "WHITC", OrderId = 10596 },
+                    new CustomerOrderDto() { CustomerId = "WHITC", OrderId = 10693 },
+                    new CustomerOrderDto() { CustomerId = "WHITC", OrderId = 10696 },
+                    new CustomerOrderDto() { CustomerId = "WHITC", OrderId = 10723 },
+                    new CustomerOrderDto() { CustomerId = "WHITC", OrderId = 10740 },
+                    new CustomerOrderDto() { CustomerId = "WHITC", OrderId = 10861 },
+                    new CustomerOrderDto() { CustomerId = "WHITC", OrderId = 10904 },
+                    new CustomerOrderDto() { CustomerId = "WHITC", OrderId = 11032 },
+                    new CustomerOrderDto() { CustomerId = "WHITC", OrderId = 11066 }
+                };
+
+            CollectionAssert.AreEqual(actual.ToList(), expected.ToList(), "You failed!");
+        }
+
+        [TestMethod]
+        public void TestTakeWhile()
+        {
+            IEnumerable<int> actual = Partitioning.TakeWhile();
+            IEnumerable<int> expected = new int[] { 5, 4, 1, 3 };
+
+            CollectionAssert.AreEqual(actual.ToList(), expected.ToList(), "You failed!");
+        }
+
+        [TestMethod]
+        public void TestTakeWhileIndexed()
+        {
+            IEnumerable<int> actual = Partitioning.TakeWhileIndexed();
+            IEnumerable<int> expected = new int[] { 5, 4 };
+
+            CollectionAssert.AreEqual(actual.ToList(), expected.ToList(), "You failed!");
+        }
+
+        [TestMethod]
+        public void TestSkipWhile()
+        {
+            IEnumerable<int> actual = Partitioning.SkipWhile();
+            IEnumerable<int> expected = new int[] { 3, 9, 8, 6, 7, 2, 0 };
+
+            CollectionAssert.AreEqual(actual.ToList(), expected.ToList(), "You failed!");
+        }
+
+        [TestMethod]
+        public void TestSkipWhileIndexed()
+        {
+            IEnumerable<int> actual = Partitioning.SkipWhileIndexed();
+            IEnumerable<int> expected = new int[] { 1, 3, 9, 8, 6, 7, 2, 0 };
+
+            CollectionAssert.AreEqual(actual.ToList(), expected.ToList(), "You failed!");
+        }
+    }
+}

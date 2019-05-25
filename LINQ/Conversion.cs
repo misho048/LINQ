efaeff416ccc5f9b098373f4492382d9e54aaa95
Kubo <jakub.berthoty@gmail.com>
2019-05-25 22:10:29 +0200

    Conversion.

---
+++ LINQ/Conversion.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace LINQ
+{
+    public static class Conversion
+    {
+        /// <summary>
+        /// Returns sequence ordered from highest number to lowest as an array.
+        /// </summary>
+        /// <returns>Array with numbers ordered in descending order.</returns>
+        public static double[] ToArray()
+        {
+            List<double> doubles = new List<double> { 1.7, 2.3, 1.9, 4.1, 2.9 };
+
+            // !!! INSERT YOUR LINQ  MAGIC HERE !!!            
+
+            return new double[] { };
+        }
+
+        /// <summary>
+        /// Returns sequence ordered alphabetically as a list.
+        /// </summary>
+        /// <returns>List with words ordered alphabetically.</returns>
+        public static List<string> ToList()
+        {
+            string[] words = { "cherry", "apple", "blueberry" };
+
+            // !!! INSERT YOUR LINQ  MAGIC HERE !!!
+            
+            return new List<string>();
+        }
+
+        /// <summary>
+        /// Returns dictionary of test results.
+        /// </summary>
+        /// <returns>Dictionary with test results where student name is the key and their score is the value.</returns>
+        public static Dictionary<string, int> ToDictionary()
+        {
+            var scoreRecords = new[] { new {Name = "Alice", Score = 50},
+                                       new {Name = "Bob"  , Score = 40},
+                                       new {Name = "Cathy", Score = 45} };
+
+            // !!! INSERT YOUR LINQ  MAGIC HERE !!!
+            
+            return new Dictionary<string, int>();
+        }
+
+        /// <summary>
+        /// Returns only elements of type double from array.
+        /// </summary>
+        /// <returns>Collection with only elements of type double.</returns>
+        public static IEnumerable<double> OfType()
+        {
+            object[] numbers = { null, 1.0, "two", 3, "four", 5, "six", 7.0 };
+
+            // !!! INSERT YOUR LINQ  MAGIC HERE !!!
+            
+            return new double[] { };
+        }
+    }
+}
---
+++ LinqTests/ConversionTest.cs
@@ -0,0 +1,47 @@
+﻿using LINQ;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Linq;
+
+namespace LinqTests
+{
+    [TestClass]
+    public class ConversionTest
+    {
+        [TestMethod]
+        public void TestToArray()
+        {
+            double[] actual = Conversion.ToArray();
+            double[] expected = new double[] { 4.1, 2.9, 2.3, 1.9, 1.7 };
+
+            CollectionAssert.AreEqual(actual, expected, "You failed!");
+        }
+
+        [TestMethod]
+        public void TestToList()
+        {
+            List<string> actual = Conversion.ToList();
+            List<string> expected = new List<string> { "apple", "blueberry", "cherry" };
+
+            CollectionAssert.AreEqual(actual, expected, "You failed!");
+        }
+
+        [TestMethod]
+        public void TestToDictionary()
+        {
+            Dictionary<string, int> actual = Conversion.ToDictionary();
+            Dictionary<string, int> expected = new Dictionary<string, int>() { { "Alice", 50 }, { "Bob", 40 }, { "Cathy", 45 } };
+
+            Assert.AreEqual(true, actual.ContentEquals(expected), "You failed!");
+        }
+
+        [TestMethod]
+        public void TestOfType()
+        {
+            IEnumerable<double> actual = Conversion.OfType();
+            IEnumerable<double> expected = new double[] { 1.0, 7.0 };
+
+            CollectionAssert.AreEqual(actual.ToList(), expected.ToList(), "You failed!");
+        }
+    }
+}

3f96014b2653779995634d31d5a033539744fafc
Jakub Berthoty <jakub.berthoty@gmail.com>
2019-05-25 18:39:25 +0200

    Set operators.

---
+++ LINQ/SetOperators.cs
@@ -0,0 +1,120 @@
+﻿using LINQ.Data;
+using LINQ.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LINQ
+{
+    public static class SetOperators
+    {
+        /// <summary>
+        /// Removes duplicate elements in a sequence of factors of 300.
+        /// </summary>
+        /// <returns>Collection with unique factors of 300.</returns>
+        public static IEnumerable<int> Distinct01()
+        {
+            int[] factorsOf300 = { 2, 2, 3, 5, 5 };
+
+            // !!! INSERT YOUR LINQ  MAGIC HERE !!!
+
+            return new int[] { };
+        }
+
+        /// <summary>
+        /// Reurns unique product category names.
+        /// </summary>
+        /// <returns>Collection with unique product category names.</returns>
+        public static IEnumerable<string> Distinct02()
+        {
+            List<Product> products = DataLoader.GetProductList();
+
+            // !!! INSERT YOUR LINQ  MAGIC HERE !!!
+
+            return new string[] { };
+        }
+
+        /// <summary>
+        /// Reurns one sequence that contains the unique values from two arrays.
+        /// </summary>
+        /// <returns>Collection with unique values from two arrays.</returns>
+        public static IEnumerable<int> Union01()
+        {
+            int[] numbersA = { 0, 2, 4, 5, 6, 8, 9 };
+            int[] numbersB = { 1, 3, 5, 7, 8 };
+
+            // !!! INSERT YOUR LINQ  MAGIC HERE !!!
+
+            return new int[] { };
+        }
+
+        /// <summary>
+        /// Reurns one sequence that contains unique first letter from both product and customer names.
+        /// </summary>
+        /// <returns>Collection with unique first letter from both product and customer names.</returns>
+        public static IEnumerable<char> Union02()
+        {
+            List<Product> products = DataLoader.GetProductList();
+            List<Customer> customers = DataLoader.GetCustomerList();
+
+            // !!! INSERT YOUR LINQ  MAGIC HERE !!!
+
+            return new char[] { };
+        }
+
+        /// <summary>
+        /// Reurns one sequence that contains the common values shared by two arrays.
+        /// </summary>
+        /// <returns>Collection with common values shared by two arrays.</returns>
+        public static IEnumerable<int> Intersect01()
+        {
+            int[] numbersA = { 0, 2, 4, 5, 6, 8, 9 };
+            int[] numbersB = { 1, 3, 5, 7, 8 };
+
+            // !!! INSERT YOUR LINQ  MAGIC HERE !!!
+
+            return new int[] { };
+        }
+
+        /// <summary>
+        /// Reurns one sequence that contains the common first letter from both product and customer names.
+        /// </summary>
+        /// <returns>Collection with common first letter from both product and customer names.</returns>
+        public static IEnumerable<char> Intersect02()
+        {
+            List<Product> products = DataLoader.GetProductList();
+            List<Customer> customers = DataLoader.GetCustomerList();
+
+            // !!! INSERT YOUR LINQ  MAGIC HERE !!!
+
+            return new char[] { };
+        }
+
+        /// <summary>
+        /// Reurns one sequence that contains the values from numbersA that are not also in numbersB.
+        /// </summary>
+        /// <returns>Collection with values from numbersA that are not also in numbersB.</returns>
+        public static IEnumerable<int> Except01()
+        {
+            int[] numbersA = { 0, 2, 4, 5, 6, 8, 9 };
+            int[] numbersB = { 1, 3, 5, 7, 8 };
+
+            // !!! INSERT YOUR LINQ  MAGIC HERE !!!
+
+            return new int[] { };
+        }
+
+        /// <summary>
+        /// Reurns one sequence that contains the first letters of product names that are not also first letters of customer names.
+        /// </summary>
+        /// <returns>Collection with first letters of product names that are not also first letters of customer names.</returns>
+        public static IEnumerable<char> Except02()
+        {
+            List<Product> products = DataLoader.GetProductList();
+            List<Customer> customers = DataLoader.GetCustomerList();
+
+            // !!! INSERT YOUR LINQ  MAGIC HERE !!!
+
+            return new char[] { };
+        }
+    }
+}
---
+++ LinqTests/SetOperatorsTest.cs
@@ -0,0 +1,95 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using LINQ;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LinqTests
+{
+    [TestClass]
+    public class SetOperatorsTest
+    {
+        [TestMethod]
+        public void TestDistinct01()
+        {
+            IEnumerable<int> actual = SetOperators.Distinct01();
+            IEnumerable<int> expected = new int[] { 2, 3, 5 };
+
+            CollectionAssert.AreEqual(actual.ToList(), expected.ToList(), "You failed!");
+        }
+
+        [TestMethod]
+        public void TestDistinct02()
+        {
+            IEnumerable<string> actual = SetOperators.Distinct02();
+            IEnumerable<string> expected =
+                new string[] {
+                    "Beverages",
+                    "Condiments",
+                    "Produce",
+                    "Meat/Poultry",
+                    "Seafood",
+                    "Dairy Products",
+                    "Confections",
+                    "Grains/Cereals"
+                };
+
+            CollectionAssert.AreEqual(actual.ToList(), expected.ToList(), "You failed!");
+        }
+
+        [TestMethod]
+        public void TestUnion01()
+        {
+            IEnumerable<int> actual = SetOperators.Union01();
+            IEnumerable<int> expected = new int[] { 0, 2, 4, 5, 6, 8, 9, 1, 3, 7 };
+
+            CollectionAssert.AreEqual(actual.ToList(), expected.ToList(), "You failed!");
+        }
+
+        [TestMethod]
+        public void TestUnion02()
+        {
+            IEnumerable<char> actual = SetOperators.Union02();
+            IEnumerable<char> expected = new char[] { 'C', 'A', 'G', 'U', 'N', 'M', 'I', 'Q', 'K', 'T', 'P', 'S', 'R', 'B',
+                                                      'J', 'Z', 'V', 'F', 'E', 'W', 'L', 'O', 'D', 'H' };
+
+            CollectionAssert.AreEqual(actual.ToList(), expected.ToList(), "You failed!");
+        }
+
+        [TestMethod]
+        public void TestIntersect01()
+        {
+            IEnumerable<int> actual = SetOperators.Intersect01();
+            IEnumerable<int> expected = new int[] { 5, 8 };
+
+            CollectionAssert.AreEqual(actual.ToList(), expected.ToList(), "You failed!");
+        }
+
+        [TestMethod]
+        public void TestIntersect02()
+        {
+            IEnumerable<char> actual = SetOperators.Intersect02();
+            IEnumerable<char> expected = new char[] { 'C', 'A', 'G', 'N', 'M', 'I', 'Q', 'K', 'T', 'P', 'S', 'R', 'B',
+                                                      'V', 'F', 'E', 'W', 'L', 'O' };
+
+            CollectionAssert.AreEqual(actual.ToList(), expected.ToList(), "You failed!");
+        }
+
+        [TestMethod]
+        public void TestExcept01()
+        {
+            IEnumerable<int> actual = SetOperators.Except01();
+            IEnumerable<int> expected = new int[] { 0, 2, 4, 6, 9 };
+
+            CollectionAssert.AreEqual(actual.ToList(), expected.ToList(), "You failed!");
+        }
+
+        [TestMethod]
+        public void TestExcept02()
+        {
+            IEnumerable<char> actual = SetOperators.Except02();
+            IEnumerable<char> expected = new char[] { 'U', 'J', 'Z' };
+
+            CollectionAssert.AreEqual(actual.ToList(), expected.ToList(), "You failed!");
+        }
+    }
+}

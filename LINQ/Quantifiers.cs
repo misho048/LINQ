6470583ca5c46eada507b9acc871795d30ea76a1
Kubo <jakub.berthoty@gmail.com>
2019-05-25 23:29:47 +0200

    Quantifiers.

---
+++ LINQ/Quantifiers.cs
@@ -0,0 +1,62 @@
+﻿using LINQ.Data;
+using LINQ.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LINQ
+{
+    public static class Quantifiers
+    {
+        /// <summary>
+        /// Checks whether any of the words in the array contains the substring 'ei'.
+        /// </summary>
+        /// <returns>True, if any of the words in the array contains the substring 'ei'; otherwise false.</returns>
+        public static bool? Any01()
+        {
+            string[] words = { "believe", "relief", "receipt", "field" };
+
+            // !!! INSERT YOUR LINQ  MAGIC HERE !!!
+            
+            return null;
+        }
+
+        /// <summary>
+        /// Returns list of product categories where at least one product is out of stock.
+        /// </summary>
+        /// <returns>Collection of product categories where at least one product is out of stock.</returns>
+        public static IEnumerable<string> Any02()
+        {
+            List<Product> products = DataLoader.GetProductList();
+
+            // !!! INSERT YOUR LINQ  MAGIC HERE !!!
+            
+            return new string[] { };
+        }
+
+        /// <summary>
+        /// Checks whether an array contains only odd numbers.
+        /// </summary>
+        /// <returns>True, if array contains only odd numbers; otherwise false.</returns>
+        public static bool? All01()
+        {
+            int[] numbers = { 1, 11, 3, 19, 41, 65, 19 };
+
+            // !!! INSERT YOUR LINQ  MAGIC HERE !!!
+            
+            return null;
+        }
+
+        /// <summary>
+        /// Returns list of product categories where every product is in stock.
+        /// </summary>
+        /// <returns>Collection of product categories where every product is in stock.</returns>
+        public static IEnumerable<string> All02()
+        {
+            List<Product> products = DataLoader.GetProductList();
+
+            // !!! INSERT YOUR LINQ  MAGIC HERE !!!            
+            
+            return new string[] { };
+        }
+    }
+}
---
+++ LinqTests/QuantifiersTest.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using LINQ;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LinqTests
+{
+    [TestClass]
+    public class QuantifiersTest
+    {
+        [TestMethod]
+        public void TestAny01()
+        {
+            bool? actual = Quantifiers.Any01();
+            bool? expected = true;
+
+            Assert.AreEqual(actual, expected, "You failed!");
+        }
+
+        [TestMethod]
+        public void TestAny02()
+        {
+            IEnumerable<string> actual = Quantifiers.Any02();
+            IEnumerable<string> expected = new string[] { "Condiments", "Meat/Poultry", "Dairy Products" };
+
+            CollectionAssert.AreEqual(actual.ToList(), expected.ToList(), "You failed!");
+        }
+
+        [TestMethod]
+        public void TestAll01()
+        {
+            bool? actual = Quantifiers.All01();
+            bool? expected = true;
+
+            Assert.AreEqual(actual, expected, "You failed!");
+        }
+
+        [TestMethod]
+        public void TestAll02()
+        {
+            IEnumerable<string> actual = Quantifiers.All02();
+            IEnumerable<string> expected = new string[] { "Beverages", "Produce", "Seafood", "Confections", "Grains/Cereals" };
+
+            CollectionAssert.AreEqual(actual.ToList(), expected.ToList(), "You failed!");
+        }
+    }
+}

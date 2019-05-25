4aee439211bfc5261378baa44b8e534e837d24c9
Kubo <jakub.berthoty@gmail.com>
2019-05-26 00:29:26 +0200

    Aggregate operators.

---
+++ LINQ/AggregateOperators.cs
@@ -0,0 +1,218 @@
+﻿using LINQ.Data;
+using LINQ.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LINQ
+{
+    public static class AggregateOperators
+    {
+        /// <summary>
+        /// Returns the number of unique factors of 300.
+        /// </summary>
+        /// <returns>Number of unique factors of 300.</returns>
+        public static int Count01()
+        {
+            int[] factorsOf300 = { 2, 2, 3, 5, 5 };
+
+            // !!! INSERT YOUR LINQ  MAGIC HERE !!!
+            
+            return 42;
+        }
+
+        /// <summary>
+        /// Returns the number of odd ints in the array.
+        /// </summary>
+        /// <returns>Number of odd ints in the array.</returns>
+        public static int Count02()
+        {
+            int[] numbers = { 5, 4, 1, 3, 9, 8, 6, 7, 2, 0 };
+
+            // !!! INSERT YOUR LINQ  MAGIC HERE !!!
+            
+            return 42;
+        }
+
+        /// <summary>
+        /// Returns list of customers and how many orders each has.
+        /// </summary>
+        /// <returns>Dictionary where key is customer ID and value is count of their orders.</returns>
+        public static Dictionary<string, int> Count03()
+        {
+            List<Customer> customers = DataLoader.GetCustomerList();
+
+            // !!! INSERT YOUR LINQ  MAGIC HERE !!!
+            
+            return new Dictionary<string, int>();
+        }
+
+        /// <summary>
+        /// Returns list of categories and how many products each has.
+        /// </summary>
+        /// <returns>Dictionary where key is category name and value is count of their products.</returns>
+        public static Dictionary<string, int> Count04()
+        {
+            List<Product> products = DataLoader.GetProductList();
+
+            // !!! INSERT YOUR LINQ  MAGIC HERE !!!
+            
+            return new Dictionary<string, int>();
+        }
+
+        /// <summary>
+        /// Returns the total of the numbers in an array.
+        /// </summary>
+        /// <returns>Total of the numbers in an array.</returns>
+        public static int Sum01()
+        {
+            int[] numbers = { 5, 4, 1, 3, 9, 8, 6, 7, 2, 0 };
+
+            // !!! INSERT YOUR LINQ  MAGIC HERE !!!
+            
+            return 0;
+        }
+
+        /// <summary>
+        /// Returns the total number of characters of all words in the array.
+        /// </summary>
+        /// <returns>Total number of characters of all words in the array.</returns>
+        public static int Sum02()
+        {
+            string[] words = { "cherry", "apple", "blueberry" };
+
+            // !!! INSERT YOUR LINQ  MAGIC HERE !!!
+            
+            return 0;
+        }
+
+        /// <summary>
+        /// Returns the total units in stock for each product category.
+        /// </summary>
+        /// <returns>Dictionary where key is category name and value is sum of their products' units.</returns>
+        public static Dictionary<string, int> Sum03()
+        {
+            List<Product> products = DataLoader.GetProductList();
+
+            // !!! INSERT YOUR LINQ  MAGIC HERE !!!
+            
+            return new Dictionary<string, int>();
+        }
+
+        /// <summary>
+        /// Returns the lowest number in an array.
+        /// </summary>
+        /// <returns>The lowest number in an array.</returns>
+        public static int Min01()
+        {
+            int[] numbers = { 5, 4, 1, 3, 9, 8, 6, 7, 2, 0 };
+
+            // !!! INSERT YOUR LINQ  MAGIC HERE !!!
+            
+            return 42;
+        }
+
+        /// <summary>
+        /// Returns the length of the shortest word in an array.
+        /// </summary>
+        /// <returns>Length of the shortest word in an array.</returns>
+        public static int Min02()
+        {
+            string[] words = { "cherry", "apple", "blueberry" };
+
+            // !!! INSERT YOUR LINQ  MAGIC HERE !!!
+            
+            return 0;
+        }
+
+        /// <summary>
+        /// Returns the cheapest price among each category's products.
+        /// </summary>
+        /// <returns>Dictionary where key is category name and value is the cheapest price among its products.</returns>
+        public static Dictionary<string, decimal> Min03()
+        {
+            List<Product> products = DataLoader.GetProductList();
+
+            // !!! INSERT YOUR LINQ  MAGIC HERE !!!
+            
+            return new Dictionary<string, decimal>();
+        }
+
+        /// <summary>
+        /// Returns the highest number in an array.
+        /// </summary>
+        /// <returns>The highest number in an array.</returns>
+        public static int Max01()
+        {
+            int[] numbers = { 5, 4, 1, 3, 9, 8, 6, 7, 2, 0 };
+
+            // !!! INSERT YOUR LINQ  MAGIC HERE !!!
+            
+            return -1;
+        }
+
+        /// <summary>
+        /// Returns the length of the longest word in an array.
+        /// </summary>
+        /// <returns>Length of the longest word in an array.</returns>
+        public static int Max02()
+        {
+            string[] words = { "cherry", "apple", "blueberry" };
+
+            // !!! INSERT YOUR LINQ  MAGIC HERE !!!
+            
+            return 0;
+        }
+
+        /// <summary>
+        /// Returns the most expensive price among each category's products.
+        /// </summary>
+        /// <returns>Dictionary where key is category name and value is the most expensive price among its products.</returns>
+        public static Dictionary<string, decimal> Max03()
+        {
+            List<Product> products = DataLoader.GetProductList();
+
+            // !!! INSERT YOUR LINQ  MAGIC HERE !!!
+            
+            return new Dictionary<string, decimal>();
+        }
+
+        /// <summary>
+        /// Returns the average of all numbers in an array.
+        /// </summary>
+        /// <returns>The average of all numbers in an array.</returns>
+        public static double Average01()
+        {
+            int[] numbers = { 5, 4, 1, 3, 9, 8, 6, 7, 2, 0 };
+
+            // !!! INSERT YOUR LINQ  MAGIC HERE !!!
+            
+            return -1;
+        }
+
+        /// <summary>
+        /// Returns the average length of the words in the array.
+        /// </summary>
+        /// <returns>Average length of the words in the array.</returns>
+        public static double Average02()
+        {
+            string[] words = { "cherry", "apple", "blueberry" };
+
+            // !!! INSERT YOUR LINQ  MAGIC HERE !!!
+            
+            return 0;
+        }
+
+        /// <summary>
+        /// Returns the average price of each category's products.
+        /// </summary>
+        /// <returns>Dictionary where key is category name and value is the average price of its products.</returns>
+        public static Dictionary<string, decimal> Average03()
+        {
+            List<Product> products = DataLoader.GetProductList();
+
+            // !!! INSERT YOUR LINQ  MAGIC HERE !!!
+            
+            return new Dictionary<string, decimal>();
+        }
+    }
+}
---
+++ LinqTests/AggregateOperatorsTest.cs
@@ -0,0 +1,303 @@
+﻿using System.Collections.Generic;
+using LINQ;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LinqTests
+{
+    [TestClass]
+    public class AggregateOperatorsTest
+    {
+        [TestMethod]
+        public void TestCount01()
+        {
+            int actual = AggregateOperators.Count01();
+            int expected = 3;
+
+            Assert.AreEqual(actual, expected, "You failed!");
+        }
+
+        [TestMethod]
+        public void TestCount02()
+        {
+            int actual = AggregateOperators.Count02();
+            int expected = 5;
+
+            Assert.AreEqual(actual, expected, "You failed!");
+        }
+
+        [TestMethod]
+        public void TestCount03()
+        {
+            Dictionary<string, int> actual = AggregateOperators.Count03();
+            Dictionary<string, int> expected =
+                new Dictionary<string, int>
+                {
+                    { "ALFKI", 6 },
+                    { "ANATR", 4 },
+                    { "ANTON", 7 },
+                    { "AROUT", 13 },
+                    { "BERGS", 18 },
+                    { "BLAUS", 7 },
+                    { "BLONP", 11 },
+                    { "BOLID", 3 },
+                    { "BONAP", 17 },
+                    { "BOTTM", 14 },
+                    { "BSBEV", 10 },
+                    { "CACTU", 6 },
+                    { "CENTC", 1 },
+                    { "CHOPS", 8 },
+                    { "COMMI", 5 },
+                    { "CONSH", 3 },
+                    { "DRACD", 6 },
+                    { "DUMON", 4 },
+                    { "EASTC", 8 },
+                    { "ERNSH", 30 } ,
+                    { "FAMIA", 7 },
+                    { "FISSA", 0 },
+                    { "FOLIG", 5 },
+                    { "FOLKO", 19 } ,
+                    { "FRANK", 15 } ,
+                    { "FRANR", 3 } ,
+                    { "FRANS", 6 } ,
+                    { "FURIB", 8 } ,
+                    { "GALED", 5 } ,
+                    { "GODOS", 10 } ,
+                    { "GOURL", 9 } ,
+                    { "GREAL", 11 } ,
+                    { "GROSR", 2 } ,
+                    { "HANAR", 14 } ,
+                    { "HILAA", 18 } ,
+                    { "HUNGC", 5 } ,
+                    { "HUNGO", 19 } ,
+                    { "ISLAT", 10 } ,
+                    { "KOENE", 14 } ,
+                    { "LACOR", 4 } ,
+                    { "LAMAI", 14 } ,
+                    { "LAUGB", 3 } ,
+                    { "LAZYK", 2 } ,
+                    { "LEHMS", 15 } ,
+                    { "LETSS", 4 } ,
+                    { "LILAS", 14 } ,
+                    { "LINOD", 12 } ,
+                    { "LONEP", 8 } ,
+                    { "MAGAA", 10 } ,
+                    { "MAISD", 7 } ,
+                    { "MEREP", 13 } ,
+                    { "MORGK", 5 } ,
+                    { "NORTS", 3 } ,
+                    { "OCEAN", 5 } ,
+                    { "OLDWO", 10 } ,
+                    { "OTTIK", 9 } ,
+                    { "PARIS", 0 } ,
+                    { "PERIC", 6 } ,
+                    { "PICCO", 10 } ,
+                    { "PRINI", 6 } ,
+                    { "QUEDE", 9 } ,
+                    { "QUEEN", 13 } ,
+                    { "QUICK", 28 } ,
+                    { "RANCH", 5 } ,
+                    { "RATTC", 18 } ,
+                    { "REGGC", 12 } ,
+                    { "RICAR", 11 } ,
+                    { "RICSU", 10 } ,
+                    { "ROMEY", 5 } ,
+                    { "SANTG", 6 } ,
+                    { "SAVEA", 31 } ,
+                    { "SEVES", 9 } ,
+                    { "SIMOB", 7 } ,
+                    { "SPECD", 4 } ,
+                    { "SPLIR", 9 } ,
+                    { "SUPRD", 12 } ,
+                    { "THEBI", 4 } ,
+                    { "THECR", 3 } ,
+                    { "TOMSP", 5 } ,
+                    { "TORTU", 10 } ,
+                    { "TRADH", 7 } ,
+                    { "TRAIH", 3 } ,
+                    { "VAFFE", 11 } ,
+                    { "VICTE", 10 } ,
+                    { "VINET", 4 } ,
+                    { "WANDK", 10 } ,
+                    { "WARTH", 15 } ,
+                    { "WELLI", 9 } ,
+                    { "WHITC", 14 } ,
+                    { "WILMK", 8 } ,
+                    { "WOLZA", 7 }
+                };
+
+            Assert.AreEqual(true, actual.ContentEquals(expected), "You failed!");
+        }
+
+        [TestMethod]
+        public void TestCount04()
+        {
+            Dictionary<string, int> actual = AggregateOperators.Count04();
+            Dictionary<string, int> expected =
+                new Dictionary<string, int>
+                {
+                    { "Beverages", 12 },
+                    { "Condiments", 12},
+                    { "Produce", 5 },
+                    { "Meat/Poultry", 6 },
+                    { "Seafood", 12 },
+                    { "Dairy Products", 10 },
+                    { "Confections", 13 },
+                    { "Grains/Cereals", 7 }
+                };
+
+            Assert.AreEqual(true, actual.ContentEquals(expected), "You failed!");
+        }
+
+        [TestMethod]
+        public void TestSum01()
+        {
+            int actual = AggregateOperators.Sum01();
+            int expected = 45;
+
+            Assert.AreEqual(actual, expected, "You failed!");
+        }
+
+        [TestMethod]
+        public void TestSum02()
+        {
+            int actual = AggregateOperators.Sum02();
+            int expected = 20;
+
+            Assert.AreEqual(actual, expected, "You failed!");
+        }
+
+        [TestMethod]
+        public void TestSum03()
+        {
+            Dictionary<string, int> actual = AggregateOperators.Sum03();
+            Dictionary<string, int> expected =
+                new Dictionary<string, int>
+                {
+                    { "Beverages", 559 },
+                    { "Condiments", 507 },
+                    { "Produce", 100 },
+                    { "Meat/Poultry", 165 },
+                    { "Seafood", 701 },
+                    { "Dairy Products", 393 },
+                    { "Confections", 386 },
+                    { "Grains/Cereals", 308 }
+                };
+
+            Assert.AreEqual(true, actual.ContentEquals(expected), "You failed!");
+        }
+
+        [TestMethod]
+        public void TestMin01()
+        {
+            int actual = AggregateOperators.Min01();
+            int expected = 0;
+
+            Assert.AreEqual(actual, expected, "You failed!");
+        }
+
+        [TestMethod]
+        public void TestMin02()
+        {
+            int actual = AggregateOperators.Min02();
+            int expected = 5;
+
+            Assert.AreEqual(actual, expected, "You failed!");
+        }
+
+        [TestMethod]
+        public void TestMin03()
+        {
+            Dictionary<string, decimal> actual = AggregateOperators.Min03();
+            Dictionary<string, decimal> expected =
+                new Dictionary<string, decimal>
+                {
+                    { "Beverages", 4.5M },
+                    { "Condiments", 10M },
+                    { "Produce", 10M },
+                    { "Meat/Poultry", 7.45M },
+                    { "Seafood", 6M },
+                    { "Dairy Products", 2.5M },
+                    { "Confections", 9.2M },
+                    { "Grains/Cereals", 7M }
+                };
+
+            Assert.AreEqual(true, actual.ContentEquals(expected), "You failed!");
+        }
+
+        [TestMethod]
+        public void TestMax01()
+        {
+            int actual = AggregateOperators.Max01();
+            int expected = 9;
+
+            Assert.AreEqual(actual, expected, "You failed!");
+        }
+
+        [TestMethod]
+        public void TestMax02()
+        {
+            int actual = AggregateOperators.Max02();
+            int expected = 9;
+
+            Assert.AreEqual(actual, expected, "You failed!");
+        }
+
+        [TestMethod]
+        public void TestMax03()
+        {
+            Dictionary<string, decimal> actual = AggregateOperators.Max03();
+            Dictionary<string, decimal> expected =
+                new Dictionary<string, decimal>
+                {
+                    { "Beverages", 263.5M },
+                    { "Condiments", 43.9M },
+                    { "Produce", 53M },
+                    { "Meat/Poultry", 123.79M },
+                    { "Seafood", 62.5M },
+                    { "Dairy Products", 55M },
+                    { "Confections", 81M },
+                    { "Grains/Cereals", 38M }
+                };
+
+            Assert.AreEqual(true, actual.ContentEquals(expected), "You failed!");
+        }
+
+        [TestMethod]
+        public void TestAverage01()
+        {
+            double actual = AggregateOperators.Average01();
+            double expected = 4.5;
+
+            Assert.AreEqual(actual, expected, "You failed!");
+        }
+
+        [TestMethod]
+        public void TestAverage02()
+        {
+            double actual = AggregateOperators.Average02();
+            double expected = 6.666666666666667;
+
+            Assert.AreEqual(actual, expected, "You failed!");
+        }
+
+        [TestMethod]
+        public void TestAverage03()
+        {
+            Dictionary<string, decimal> actual = AggregateOperators.Average03();
+            Dictionary<string, decimal> expected =
+                new Dictionary<string, decimal>
+                {
+                    { "Beverages", 37.979166666666666666666666667M },
+                    { "Condiments", 23.0625M },
+                    { "Produce", 32.3700M },
+                    { "Meat/Poultry", 54.006666666666666666666666667M },
+                    { "Seafood", 20.6825M },
+                    { "Dairy Products", 28.7300M },
+                    { "Confections", 25.1600M },
+                    { "Grains/Cereals", 20.2500M }
+                };
+
+            Assert.AreEqual(true, actual.ContentEquals(expected), "You failed!");
+        }
+    }
+}

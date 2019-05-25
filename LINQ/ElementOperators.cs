c397e74f949bb3608891a0b63ef562bfc92223df
Kubo <jakub.berthoty@gmail.com>
2019-05-25 22:23:55 +0200

    Element operators.

---
+++ LINQ/ElementOperators.cs
@@ -0,0 +1,75 @@
+﻿using LINQ.Data;
+using LINQ.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LINQ
+{
+    public static class ElementOperators
+    {
+        /// <summary>
+        /// Returns first product from Confections category.
+        /// </summary>
+        /// <returns>First product from Confections category.</returns>
+        public static Product First01()
+        {
+            List<Product> products = DataLoader.GetProductList();
+
+            // !!! INSERT YOUR LINQ  MAGIC HERE !!!            
+            
+            return null;
+        }
+
+        /// <summary>
+        /// Returns first element in the array that starts with 'o'.
+        /// </summary>
+        /// <returns>First element in the array that starts with 'o'.</returns>
+        public static string First02()
+        {
+            string[] strings = { "zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine" };
+
+            // !!! INSERT YOUR LINQ  MAGIC HERE !!!            
+            
+            return string.Empty;
+        }
+
+        /// <summary>
+        /// Tries to return first or default element from an array.
+        /// </summary>
+        /// <returns>First or default element from an array.</returns>
+        public static int FirstOrDefault01()
+        {
+            int[] numbers = { };
+
+            // !!! INSERT YOUR LINQ  MAGIC HERE !!!            
+            
+            return 42;
+        }
+
+        /// <summary>
+        /// Tries to return first or default product with ID 789.
+        /// </summary>
+        /// <returns>First or default product with ID 789.</returns>
+        public static Product FirstOrDefault02()
+        {
+            List<Product> products = DataLoader.GetProductList();
+
+            // !!! INSERT YOUR LINQ  MAGIC HERE !!!            
+            
+            return new Product();
+        }
+
+        /// <summary>
+        /// Returns second number greater than 5 from an array.
+        /// </summary>
+        /// <returns>Second number greater than 5 from an array.</returns>
+        public static int ElementAt()
+        {
+            int[] numbers = { 5, 4, 1, 3, 9, 8, 6, 7, 2, 0 };
+
+            // !!! INSERT YOUR LINQ  MAGIC HERE !!!            
+            
+            return 42;
+        }
+    }
+}
---
+++ LinqTests/ElementOperatorTest.cs
@@ -0,0 +1,55 @@
+﻿using LINQ;
+using LINQ.Models;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LinqTests
+{
+    [TestClass]
+    public class ElementOperatorTest
+    {
+        [TestMethod]
+        public void TestFirst01()
+        {
+            Product actual = ElementOperators.First01();
+            Product expected = new Product { ProductID = 16, ProductName = "Pavlova", Category = "Confections", UnitPrice = 17.4500M, UnitsInStock = 29 };
+
+            Assert.AreEqual(actual, expected, "You failed!");
+        }
+
+        [TestMethod]
+        public void TestFirst02()
+        {
+            string actual = ElementOperators.First02();
+            string expected = "one";
+
+            Assert.AreEqual(actual, expected, "You failed!");
+        }
+
+        [TestMethod]
+        public void TestFirstOrDefault01()
+        {
+            int actual = ElementOperators.FirstOrDefault01();
+            int expected = 0;
+
+            Assert.AreEqual(actual, expected, "You failed!");
+        }
+
+        [TestMethod]
+        public void TestFirstOrDefault02()
+        {
+            Product actual = ElementOperators.FirstOrDefault02();
+            Product expected = null;
+
+            Assert.AreEqual(actual, expected, "You failed!");
+        }
+
+        [TestMethod]
+        public void TestElementAt()
+        {
+            int actual = ElementOperators.ElementAt();
+            int expected = 8;
+
+            Assert.AreEqual(actual, expected, "You failed!");
+        }
+    }
+}
